48d16ce51f02080a156622c84c88957757045ad6
SimoLieF <55975272+SimoLieF@users.noreply.github.com>
2019-10-07 04:40:10 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,16 +4,16 @@ int main(int argc, char *argv[])
 {
     char str[5];
     char Num[100][5];
-    int cnt;
+    int arrayNum;
     
     
     while(fgets(str, sizeof(str), stdin)){
         if(str>="0"){
             Num[cnt]=str;
-            cnt++;
         }
-        else if(str<"0")
-        printf("%s", Num[str]);
+        else if(str<"0"){
+            arrayNum=ctoi(str)*(-1);
+        printf("%s", Num[arrayNum]);
     }
 
     return 0;
