e75c9c9ce71b360f6acb418e0b1081af40980b8c
SimoLieF <55975272+SimoLieF@users.noreply.github.com>
2019-10-07 04:58:45 +0900

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     
     while(fgets(str, sizeof(str), stdin)){
         if(str>="0"){
-            Num[cnt]=str;
+            snprintf(Num[cnt],str);
             cnt++;
         }
         else if(str<"0"){
